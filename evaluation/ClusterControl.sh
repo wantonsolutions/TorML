5a402d612d6ee58506d4b2ca25c7db5cfc3e1778
wantonsolutions <stewbertgrant@gmail.com>
2017-10-19 20:07:44 -0700

    automation scripts

--- evaluation/ClusterControl.sh
+++ evaluation/ClusterControl.sh
@@ -2,14 +2,27 @@
 
 vms[0]=40.86.185.245
 
+bootstrap='rm TorMentorAzureInstall.sh; wget https://raw.githubusercontent.com/wantonsolutions/TorML/master/evaluation/TorMentorAzureInstall.sh; chmod 755 TorMentorAzureInstall.sh; sudo sh -c "yes | ./TorMentorAzureInstall.sh"'
 pingcommand='ping -c 5 198.162.52.147 | tail -1 | cut -d / -f 5 > $HOSTNAME.ping'
 pinglocation="/home/stew/*ping"
+
+sysdir='go/src/github.com/wantonsolutions/TorML/DistSys/'
+
+runclient='tor & sleep 3;cd go/src/github.com/wantonsolutions/TorML/DistSys/; go build torclient.go; ./torclient $HOSTNAME models credit1'
 
 function onall {
     echo running $1
     for vm in ${vms[@]}
     do
         ssh stew@$vm -x $1 
+    done
+}
+
+function onallasync {
+    echo running $1
+    for vm in ${vms[@]}
+    do
+        ssh stew@$vm -x $1 &
     done
 }
 
@@ -22,6 +35,7 @@ function getall {
 }
 
 
-
+#onall "$bootstrap"
 onall "$pingcommand"
+onall "$runclient"
 getall "$pinglocation" ./
