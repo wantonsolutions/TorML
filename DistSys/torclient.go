44c71bce28ab40cffc15e8c6c1330c311687b275
wantonsolutions <stewbertgrant@gmail.com>
2017-10-22 18:59:54 -0700

    client fix

--- DistSys/torclient.go
+++ DistSys/torclient.go
@@ -471,7 +471,7 @@ func constructAddress(host string, port int) string {
 func checkError(err error) {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
-		os.Exit(1)
+		//os.Exit(1)
 	}
 }
 
